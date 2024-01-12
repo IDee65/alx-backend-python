5fcddc0024dfbc95c8e7b251d62a2768760dbd36
Idemudia <idemudia.agbonghae@iisysgroup.com>
2024-01-12 07:15:09 +0100

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Floor module """
+
+
+def floor(n: float) -> int:
+    """ Returns the floor of the float
+
+    Args:
+        n (float): float
+
+    Returns:
+        int: floor of the float
+    """
+    return int(n)
