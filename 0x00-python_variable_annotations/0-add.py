d53ca8e55947a161a64548ffbcc656f4e850f8bc
Idemudia <idemudia.agbonghae@iisysgroup.com>
2024-01-12 06:57:24 +0100

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Add module
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Adds two floating numbers
+
+    Args:
+        a (float): a
+        b (float): b
+
+    Returns:
+        float: result
+    """
+    result: float = a + b
+    return result
