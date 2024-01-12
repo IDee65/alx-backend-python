751615f08618d0a0626c75b2b0dc8dc3e974d9ad
Idemudia <idemudia.agbonghae@iisysgroup.com>
2024-01-12 07:15:02 +0100

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Concat module """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenates two strings
+
+    Args:
+        str1 (str): str1
+        str2 (str): str2
+
+    Returns:
+        str: result
+    """
+    result: str = str1 + str2
+    return result
