ece5f88ffdb7c810ef462b3e02a82d655bcc322b
Idemudia <idemudia.agbonghae@iisysgroup.com>
2024-01-12 07:15:16 +0100

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of the float
+
+    Args:
+        n (float): float
+
+    Returns:
+        str: string representation of the float
+    """
+    return str(n)
